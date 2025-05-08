0e918e3af16852fdc6fcab06968d89605a7a5a69
Vulcalien <vulcalien@vulcalien.net>
2025-05-08 11:10:00 +0200

    Add basic structure of the new single program

---
+++ src/main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+#include <pthread.h>
+
+#include "processing.h"
+#include "receiver.h"
+#include "sender.h"
+
+int main(int argc, char *argv[]) {
+    pthread_t processing_thread;
+    pthread_t receiver_thread;
+    pthread_t sender_thread;
+
+    // TODO handle returned errors
+    pthread_create(&processing_thread, NULL, processing_run, NULL);
+    pthread_create(&receiver_thread,   NULL, receiver_run,   NULL);
+    pthread_create(&sender_thread,     NULL, sender_run,     NULL);
+
+    return 0;
+}
