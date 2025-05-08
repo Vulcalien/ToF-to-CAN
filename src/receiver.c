4bafd3420cca5d86c6220c28ea60e08681198536
Vulcalien <vulcalien@vulcalien.net>
2025-05-08 11:10:29 +0200

    Receiver: add basic structure of module

---
+++ src/receiver.c
@@ -0,0 +1,33 @@
+#include "receiver.h"
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <nuttx/can/can.h>
+
+static void handle_message(struct can_msg_s *msg) {
+    // TODO ignore confirmation messages???
+    /*if(msg->cm_hdr.tcf)*/
+        /*return;*/
+
+    switch(msg->cm_hdr.ch_id) {
+        // TODO
+    }
+}
+
+void *receiver_run(void *arg) {
+    int can_fd = open("/dev/can0", O_RDONLY | O_NOCTTY);
+    if(can_fd < 0) {
+        perror("Receiver: /dev/can0 open");
+        printf("Receiver: stopping");
+        return NULL;
+    }
+
+    // TODO bit timing
+
+    while(true) {
+        struct can_msg_s msg;
+        // TODO read can_fd
+
+        handle_message(&message);
+    }
+}
