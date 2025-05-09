18ea826d7daade475a736e42ce781c162b9d73ab
Vulcalien <vulcalien@vulcalien.net>
2025-05-09 21:45:50 +0200

    Add header defining CAN interface

---
+++ include/distance-sensor.h
@@ -0,0 +1,28 @@
+#ifndef DISTANCE_SENSOR_CAN_INTERFACE
+#define DISTANCE_SENSOR_CAN_INTERFACE
+
+struct distance_sensor_can_config {
+    // ToF settings
+    unsigned int resolution_sqrt   : 8; // 4 or 8
+    unsigned int ranging_frequency : 8; // 1...60Hz
+    unsigned int ranging_mode      : 1; // 0=autonomous, 1=continuous
+    unsigned int sharpener         : 7; // 0...99%
+
+    // processing settings
+    unsigned int processing_mode : 8;  // TODO explain this field
+    unsigned int threshold       : 16; // 0...4000mm
+    unsigned int threshold_delay : 8;  // 0...255
+
+    // data transmission
+    // TODO
+};
+
+struct distance_sensor_can_sample {
+    unsigned short distance;
+    bool below_threshold;
+};
+
+#define DISTANCE_SENSOR_CAN_CONFIG_MASK_ID 0x6c0 // 0x6c0...0x6df
+#define DISTANCE_SENSOR_CAN_SAMPLE_MASK_ID 0x6e0 // 0x6e0...0x6ff
+
+#endif // DISTANCE_SENSOR_CAN_INTERFACE
