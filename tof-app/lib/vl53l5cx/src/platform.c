57ecba6e87fd8131a907a6e6c11bde8998bbf3d0
Vulcalien <vulcalien@vulcalien.net>
2025-05-11 11:32:51 +0200

    Add implementation for some of vl53l5cx's platform functions

--- tof-app/lib/vl53l5cx/src/platform.c
+++ tof-app/lib/vl53l5cx/src/platform.c
@@ -10,106 +10,114 @@
   ******************************************************************************
   */
 
-
-
 #include "platform.h"
 
-uint8_t VL53L5CX_RdByte(
-		VL53L5CX_Platform *p_platform,
-		uint16_t RegisterAdress,
-		uint8_t *p_value)
-{
-	uint8_t status = 255;
-	
-	/* Need to be implemented by customer. This function returns 0 if OK */
+#include <unistd.h>
 
-	return status;
+#include <nuttx/i2c/i2c_master.h>
+
+#define I2C_FREQUENCY 400000 // 400 KHz
+
+extern struct i2c_master_s *i2cmain;
+
+uint8_t VL53L5CX_RdByte(
+        VL53L5CX_Platform *p_platform,
+        uint16_t RegisterAdress,
+        uint8_t *p_value)
+{
+    struct i2c_config_s config;
+    config.frequency = I2C_FREQUENCY;
+    config.address   = p_platform->address;
+    config.addrlen   = 7;
+
+    uint8_t buf[2];
+    buf[0] = (RegisterAdress >> 8) & 0xff;
+    buf[1] = (RegisterAdress)      & 0xff;
+
+    return i2c_writeread(i2cmain, &config, &buf, 2, &p_value, 1);
 }
 
 uint8_t VL53L5CX_WrByte(
-		VL53L5CX_Platform *p_platform,
-		uint16_t RegisterAdress,
-		uint8_t value)
+        VL53L5CX_Platform *p_platform,
+        uint16_t RegisterAdress,
+        uint8_t value)
 {
-	uint8_t status = 255;
+    struct i2c_config_s config;
+    config.frequency = I2C_FREQUENCY;
+    config.address   = p_platform->address;
+    config.addrlen   = 7;
 
-	/* Need to be implemented by customer. This function returns 0 if OK */
+    uint8_t buf[3];
+    buf[0] = (RegisterAdress >> 8) & 0xff;
+    buf[1] = (RegisterAdress)      & 0xff;
+    buf[2] = value;
 
-	return status;
+    return i2c_write(i2cmain, &config, buf, 3);
 }
 
 uint8_t VL53L5CX_WrMulti(
-		VL53L5CX_Platform *p_platform,
-		uint16_t RegisterAdress,
-		uint8_t *p_values,
-		uint32_t size)
+        VL53L5CX_Platform *p_platform,
+        uint16_t RegisterAdress,
+        uint8_t *p_values,
+        uint32_t size)
 {
-	uint8_t status = 255;
-	
-		/* Need to be implemented by customer. This function returns 0 if OK */
+    // TODO
+    uint8_t status = 255;
 
-	return status;
+        /* Need to be implemented by customer. This function returns 0 if OK */
+
+    return status;
 }
 
 uint8_t VL53L5CX_RdMulti(
-		VL53L5CX_Platform *p_platform,
-		uint16_t RegisterAdress,
-		uint8_t *p_values,
-		uint32_t size)
+        VL53L5CX_Platform *p_platform,
+        uint16_t RegisterAdress,
+        uint8_t *p_values,
+        uint32_t size)
 {
-	uint8_t status = 255;
-	
-	/* Need to be implemented by customer. This function returns 0 if OK */
-	
-	return status;
+    // TODO
+    uint8_t status = 255;
+
+    /* Need to be implemented by customer. This function returns 0 if OK */
+
+    return status;
 }
 
 uint8_t VL53L5CX_Reset_Sensor(
-		VL53L5CX_Platform *p_platform)
+        VL53L5CX_Platform *p_platform)
 {
-	uint8_t status = 0;
-	
-	/* (Optional) Need to be implemented by customer. This function returns 0 if OK */
-	
-	/* Set pin LPN to LOW */
-	/* Set pin AVDD to LOW */
-	/* Set pin VDDIO  to LOW */
-	VL53L5CX_WaitMs(p_platform, 100);
+    // TODO
+    uint8_t status = 0;
 
-	/* Set pin LPN of to HIGH */
-	/* Set pin AVDD of to HIGH */
-	/* Set pin VDDIO of  to HIGH */
-	VL53L5CX_WaitMs(p_platform, 100);
+    /* (Optional) Need to be implemented by customer. This function returns 0 if OK */
 
-	return status;
+    /* Set pin LPN to LOW */
+    /* Set pin AVDD to LOW */
+    /* Set pin VDDIO  to LOW */
+    VL53L5CX_WaitMs(p_platform, 100);
+
+    /* Set pin LPN of to HIGH */
+    /* Set pin AVDD of to HIGH */
+    /* Set pin VDDIO of  to HIGH */
+    VL53L5CX_WaitMs(p_platform, 100);
+
+    return status;
 }
 
 void VL53L5CX_SwapBuffer(
-		uint8_t 		*buffer,
-		uint16_t 	 	 size)
+        uint8_t         *buffer,
+        uint16_t         size)
 {
-	uint32_t i, tmp;
-	
-	/* Example of possible implementation using <string.h> */
-	for(i = 0; i < size; i = i + 4) 
-	{
-		tmp = (
-		  buffer[i]<<24)
-		|(buffer[i+1]<<16)
-		|(buffer[i+2]<<8)
-		|(buffer[i+3]);
-		
-		memcpy(&(buffer[i]), &tmp, 4);
-	}
-}	
+    for(uint32_t i = 0; i < size; i += 4) {
+        uint32_t tmp = buffer[i]     << 24 | buffer[i + 1] << 16 |
+                       buffer[i + 2] << 8  | buffer[i + 3];
+        *((uint32_t *) &buffer[i]) = tmp;
+    }
+}
 
 uint8_t VL53L5CX_WaitMs(
-		VL53L5CX_Platform *p_platform,
-		uint32_t TimeMs)
+        VL53L5CX_Platform *p_platform,
+        uint32_t TimeMs)
 {
-	uint8_t status = 255;
-
-	/* Need to be implemented by customer. This function returns 0 if OK */
-	
-	return status;
+    return usleep(TimeMs * 1000);
 }
