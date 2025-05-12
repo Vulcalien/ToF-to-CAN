4442debd020c1b0fe6f94e8e1025ec653eba1832
Vulcalien <vulcalien@vulcalien.net>
2025-05-12 20:45:54 +0200

    Add small library that implements binary semaphores

---
+++ tof-app/lib/binarysem/binarysem.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <pthread.h>
+
+typedef struct {
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+
+    int val;
+} binarysem;
+
+extern int binarysem_init(binarysem *sem, int initial_value);
+extern int binarysem_destroy(binarysem *sem);
+
+extern int binarysem_post(binarysem *sem);
+extern int binarysem_wait(binarysem *sem);
