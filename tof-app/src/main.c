c54c2497c603e8a5688dfdc4d2e2b9fd69a137db
Vulcalien <vulcalien@vulcalien.net>
2025-05-13 15:47:42 +0200

    Move initialization of ToF to main.c

--- tof-app/src/main.c
+++ tof-app/src/main.c
@@ -4,8 +4,11 @@
 
 #include "processing.h"
 #include "can-io.h"
+#include "tof.h"
 
 int main(int argc, char *argv[]) {
+    while(tof_init())
+        printf("[Main] ToF initialization failed: retrying\n");
     processing_init();
 
     can_io_start();
