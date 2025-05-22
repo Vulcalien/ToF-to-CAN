fd9484c7ea537c7ed04b3191f09e35fc21e4769a
Vulcalien <vulcalien@vulcalien.net>
2025-05-22 11:01:26 +0200

    Remove unnecessary program.sh file

--- program.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-openocd \
-    -s /usr/share/openocd/scripts \
-        -f interface/stlink-v2-1.cfg \
-        -f target/stm32f7x.cfg \
-     -c "program submodules/nuttx/nuttx verify reset exit"
