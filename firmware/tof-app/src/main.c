38d40f03fe63f3e157d8f6e832271de9115a2094
Vulcalien <vulcalien@vulcalien.net>
2025-12-10 19:37:12 +0100

    Firmware: update NuttX and its builtin apps
    This should fix a build error that happens when first building the
    firmware.

--- firmware/tof-app/src/main.c
+++ firmware/tof-app/src/main.c
@@ -37,7 +37,7 @@ static int cmd_help(char *arg0) {
     return 0;
 }
 
-int main(int argc, char *argv[]) {
+int tof_app_main(int argc, char *argv[]) {
     char *arg0 = (argc > 0 ? argv[0] : "<PROGRAM-NAME>");
 
     while(tof_init())
